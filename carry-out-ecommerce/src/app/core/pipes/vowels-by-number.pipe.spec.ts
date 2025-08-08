fa2196d5eea1b66d34af13aea3e6dbc3f0ce1cfb
NestorGuzman182 <ing.nestor.guzman@gmail.com>
2025-08-08 14:40:05 -0500

    add custom lazy loads

--- carry-out-ecommerce/src/app/core/pipes/vowels-by-number.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VowelsByNumberPipe } from './vowels-by-number.pipe';
-
-describe('VowelsByNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new VowelsByNumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
